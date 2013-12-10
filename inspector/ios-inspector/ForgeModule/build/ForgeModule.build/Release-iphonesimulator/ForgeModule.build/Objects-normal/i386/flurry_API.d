cb7dc72cb61de3804023bd0a0110b6059e485ba2
Connor Dunn <connor@trigger.io>
2013-12-10 17:29:09 +0000

    Update .gitignore and remove ignored files.

--- inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/flurry_API.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/james/Code/modules/flurry/inspector/ios-inspector/ForgeModule/flurry_API.m \
-  /Users/james/Code/modules/flurry/inspector/ios-inspector/ForgeModule/flurry_API.h \
-  /Users/james/Code/modules/flurry/inspector/ios-inspector/ForgeModule/Flurry.h
