e26d28ea6f9eabcdd7a79d2c07955a38ef91729e
Connor Dunn <connor@trigger.io>
2013-12-10 17:10:58 +0000

    Remove ignored files

--- inspector/an-inspector.2013-09-19T14-14-11.544761/ForgeInspector/assets/forge/tests/automated/flurry.js
+++
@@ -1,3 +0,0 @@
-$(function () {
-	forge.flurry.setLocation({"speed":null,"heading":null,"altitudeAccuracy":null,"accuracy":24,"altitude":null,"longitude":-0.0858582,"latitude":51.523024199999995});
-});
